f3fba52691cb236ba6aec87b66a1c4b8c7a951b4
EduardoDreiss <reisedardo@gmail.com>
2026-07-25 21:02:44 -0300

    -limpeza do código

--- assets/js/services/usuarioService.js
+++ assets/js/services/usuarioService.js
@@ -1,56 +1,8 @@
-// import { supabase } from "../config/supabase.js";
-
-
-// //criação do usuário
-
-// export async function criarUsuario(usuario) {
-    
-
-//     const {data, error } = await supabase
-//         .from("participantes")
-//         .insert([usuario])
-//         .select()
-//         .single();
-
-//     if (error){
-//         console.log("erro ao cadastrar usuário: ", error);
-//         throw error;
-//     }
-
-//     return data;
-// }
-
-// export async function buscarUsuariopPorID(id) {
-
-//     const {data, error} = await supabase
-//         .from("participantes")
-//         .select("*")
-//         .eq("id", id)
-//         .single();
-
-//         if (error){
-//             console.log("erro ao buscar usuário: ", error);
-//             throw error;
-//         }
-    
-//         return data;
-    
-// }
-
 import { supabase } from "../config/supabase.js";
 
 export async function criarUsuario(usuario) {
 
     console.log("Enviando para o Supabase:", usuario);
-
-    // const { data, error } = await supabase
-    //     .from("participantes")
-    //     .insert([usuario])
-    //     .select()
-    //     .single();
-
-    // console.log("Data:", data);
-    // console.log("Error:", error);
 
     const { data, error } = await supabase
     .from("participantes")
