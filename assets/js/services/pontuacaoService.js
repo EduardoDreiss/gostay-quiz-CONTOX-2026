3127c95e78e66ae7b58197895df5180be8e4cb8d
EduardoDreiss <reisedardo@gmail.com>
2026-08-01 13:35:00 -0300

    adição das funcionalidades de pesquisa do ranking global e pessoal

--- assets/js/services/pontuacaoService.js
+++ assets/js/services/pontuacaoService.js
@@ -1,33 +1,3 @@
-// import { supabase } from "../config/supabase.js";
-
-// export async function atualizarPontuacao(id, resultado){
-
-//     const { data, error } = await supabase
-
-//         .from("participantes")
-
-//         .update({
-
-//             acertos: resultado.acertos,
-
-//             tempo_total: resultado.tempo
-
-//         })
-
-//         .eq("id", id)
-
-//         .select()
-
-//         .single();
-
-//     if(error){
-//         throw error;
-//     }
-
-//     return data;
-
-// }
-
 import { supabase } from "../config/supabase.js";
 
 export async function atualizarPontuacao(id, resultado) {
@@ -44,6 +14,70 @@ export async function atualizarPontuacao(id, resultado) {
         .eq("id", id)
         .select() // <-- Essencial para retornar a linha atualizada
         .single();
+
+    if (error) {
+        throw error;
+    }
+
+    return data;
+}
+
+
+//funcao apenas para trazer a pontuação do usuario para a pag
+//de resultados. 
+
+export async function pesquisarPontuacao(id) {
+    if (!id){
+        throw new Error("ID do partifipante não informado");
+    }
+
+    const { data, error } = await supabase
+    .from("participantes")
+    .select("acertos, tempo_total")
+    .eq("id", id)
+    .single();
+
+    if(error){
+        throw error;
+    }
+
+    return data;
+
+
+}
+
+export async function buscarRankingHoje() {
+
+    const hoje = new Date().toISOString().split("T")[0];
+
+    const { data, error } = await supabase
+        .from("participantes")
+        .select("nome, acertos, tempo_total")
+        .gte("data_cadastro", `${hoje}T00:00:00`)
+        .lt("data_cadastro", `${hoje}T23:59:59`)
+        .order("acertos", { ascending: false })
+        .order("tempo_total", { ascending: true })
+        .limit(3);
+
+    if (error) {
+        throw error;
+    }
+
+    return data;
+
+}
+
+export async function buscarRankingCompletoHoje() {
+
+    const hoje = new Date().toISOString().split("T")[0];
+
+    const { data, error } = await supabase
+        .from("participantes")
+        .select("id, nome, acertos, tempo_total")
+        .gte("data_cadastro", `${hoje}T00:00:00`)
+        .lt("data_cadastro", `${hoje}T23:59:59`)
+        .order("acertos", { ascending: false })
+        .order("tempo_total", { ascending: true });
 
     if (error) {
         throw error;
